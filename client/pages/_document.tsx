11f90e14f9f6699b0391b91e990c1394acc77236
piotrmigas <piotr.migas@icloud.com>
2024-11-06 14:49:46 +0100

    remove cdn

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -7,7 +7,6 @@ export default function Document() {
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link href='https://fonts.googleapis.com/css2?family=Work+Sans&display=swap' rel='stylesheet' />
-        <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.15.4/css/all.css' />
       </Head>
       <body>
         <Main />
